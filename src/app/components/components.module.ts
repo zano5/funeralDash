6b0021e035947326746eeef9ade7733e9391299c
Theo3668 <mtswenitheo@gmail.com>
2020-01-09 14:39:13 +0200

    first commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { NavbarComponent } from "./navbar/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+@NgModule({
+  imports: [CommonModule, RouterModule, NgbModule],
+  declarations: [NavbarComponent, SidebarComponent],
+  exports: [NavbarComponent, SidebarComponent]
+})
+export class ComponentsModule {}
